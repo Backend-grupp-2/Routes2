2043794a871704debfe3ea68714616182772caa6
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-03-05 18:52:26 +0100

    modified name in maven

--- src/main/java/org/example/routes/controller/RouteController.java
+++ src/main/java/org/example/routes/controller/RouteController.java
@@ -4,6 +4,8 @@ import org.example.routes.model.Route;
 import org.example.routes.model.RouteObj;
 import org.example.routes.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +20,20 @@ public class RouteController {
     @Autowired
     private RouteService routeService;
 
-    @GetMapping("/route/{start}/{end}/{transportType}")
+    /*@GetMapping("/route/{start}/{end}/{transportType}")
     public RouteObj getRoute(@PathVariable String start, @PathVariable String end, @PathVariable String transportType) {
         List<Route> routes = routeService.getRoute(start, end, transportType);
         RouteObj obj = new RouteObj();
         obj.setRoutes(routes);
         return obj;
+    }*/
+
+    @GetMapping("/route/{start}/{end}/{transportType}")
+    public ResponseEntity<RouteObj> getRoute(@PathVariable String start, @PathVariable String end, @PathVariable String transportType) {
+        List<Route> routes = routeService.getRoute(start, end, transportType);
+        RouteObj obj = new RouteObj();
+        obj.setRoutes(routes);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(obj);
     }
 
     /*@GetMapping("/route/{transportType}")
