c3ca132bcd1a3ce7fa409b84bb70d704ef48d318
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-03-05 17:15:13 +0100

    added properties to application.properties to try to run the server locally

--- src/main/java/org/example/routes/controller/RouteController.java
+++ src/main/java/org/example/routes/controller/RouteController.java
@@ -4,8 +4,6 @@ import org.example.routes.model.Route;
 import org.example.routes.model.RouteObj;
 import org.example.routes.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
